bf1f32a37731546c7a226d41839d5e6a8a7e3801
zhangchengxian <18746418176@163.com>
2024-01-03 19:25:08 +0800

    english

--- DB/db_graph.py
+++ DB/db_graph.py
@@ -5,12 +5,16 @@ import pickle, csv
 plt.rcParams['font.sans-serif'] = ['STFangsong']
 from sentence_transformers import SentenceTransformer
 import string
+from db_infer import loadDB
+from deep_translator import MyMemoryTranslator
 
 def get_vector(model, sentence):
     vector = model.encode(sentence)
+    '''
     with open('vector.csv', 'a', encoding='utf-8') as f:
         writer = csv.writer(f)
         writer.writerow([sentence, vector])
+    '''
     return vector
 
 def read_csv(file_path):
@@ -36,6 +40,51 @@ def add_atc_db(model, atc, db):
             db.add_edge(father, row['atc_name'])
     return db
 
+def add_atc_db_eng(model, atc, db):
+    for index, row in atc.iterrows():
+        eng_name = row['en_name']
+        if pd.isnull(row['en_name']):
+            eng_name = row['atc_name']
+        if not eng_name in db:
+            vector = get_vector(model, eng_name)
+            db.add_node(eng_name, vector=vector)
+        if row['parent_atc_code'] == '0':
+            continue
+        father = atc[atc['atc_code'] == row['parent_atc_code']]
+        if len(father) == 0:
+            continue
+        father_eng = father.iloc[0,5]
+        if pd.isnull(father_eng):
+            father_eng = father.iloc[0, 2]
+        print(father_eng)
+        if not father_eng in db:
+            father_vector = get_vector(model, father_eng)
+            db.add_node(father_eng, vector=father_vector)
+        if father_eng != eng_name:
+            db.add_edge(father_eng, eng_name)
+    return db
+
+def add_family_db_eng(model, family, db):
+    for index, row in family.iterrows():
+        eng_name = row['drug_en_name']
+        if not eng_name in db:
+            vector = get_vector(model, eng_name)
+            db.add_node(eng_name, vector=vector)
+        if row['parent_id'] == '0':
+            continue
+        father = family[family['id'] == row['parent_id']]
+        if father.shape[0] > 0:
+            father_eng = father.iloc[0, 3]
+            print(father_eng)
+        else:
+            continue
+        if not father_eng in db:
+            father_vector = get_vector(model, father_eng)
+            db.add_node(father_eng, vector=father_vector)
+        if father_eng != eng_name:
+            db.add_edge(father_eng, eng_name)
+    return db
+
 def add_family_db(model, family, db):
     for index, row in family.iterrows():
         if not row['drug_name'] in db:
@@ -52,17 +101,24 @@ def add_family_db(model, family, db):
     return db
 
 def db_save(db):
-    pickle.dump(db, open('drug_db_add_api_1117.pickle', 'wb'))
+    pickle.dump(db, open('drug_db_eng_1228.pickle', 'wb'))
 
 def read_xlsx_api(file_path):
-    df = pd.read_excel(io=file_path, dtype=str)
-    return df[['药物名称','API别名']]  
+    df = pd.read_excel(io=file_path, dtype=str, keep_default_na=False)
+    return df[["drug_name", "一级目录", "二级目录", "三级目录", "四级目录", "基础库别名"]]  
 
 def add_api(model, api, db):
     for idx,data in api.iterrows():
-        if pd.isnull(data[1]):
-            continue
-        data = [data[0]] + data[1].strip(string.punctuation).split(',')
+        res = []
+        for i in range(6):
+            if data[i] == '' or data[i] == '\\':
+                res.append([])
+            elif i != 5:
+                res.append([data[i]])
+            else:
+                res.append(data[i].rstrip(',').split(','))
+        data = res
+        '''
         for node in db.nodes():
             if db.nodes[node] in data:
                 print('add ' + node)
@@ -72,22 +128,46 @@ def add_api(model, api, db):
                         db.add_node(d, vector=get_vector(model, d))
                         for nei in neighbor:
                             db.add_edge(d, nei)
+        ''' 
+        rem = ''
+        for i in range(len(data)):
+            if len(data[i]) != 0:
+                if len(data[i]) == 1:
+                    if data[i][0] not in db.nodes():
+                        db.add_node(data[i][0], vector=get_vector(model, data[i][0]))
+                    if i == 0:
+                        rem = data[0][0]
+                    else:
+                        db.add_edge(data[i][0], rem)
+                        rem = data[i][0]
+                else:
+                    for j in range(len(data[i])):
+                        if data[i][j] not in db.nodes():
+                            db.add_node(data[i][j], vector=get_vector(model, data[i][j]))
+                        db.add_edge(data[i][j], rem)
+                    rem = ''
+
     return db
     
                         
 def main():
+    
     atc = read_csv('../../../data/drug/data_base/gene_drug_atc.csv')
     family = read_csv('../../../data/drug/data_base/gene_drug_family.csv')
     db = nx.DiGraph()
     model = SentenceTransformer('../../../model/biobert-v1.1')
-    api = read_xlsx_api('../../../data/drug/data_base/待提取异名API20231117-1.xlsx')
-    db = add_atc_db(model, atc, db)
-    db = add_family_db(model, family, db)
-    db = add_api(model, api, db)
+    #api = read_xlsx_api('../../../data/drug/data_base/待提取异名API20231117-1.xlsx')
+    #db = add_atc_db(model, atc, db)
+    #db = add_family_db(model, family, db)
+    #db = add_api(model, api, db)
     # nx.draw(db,node_size = 30, with_labels=True)
     # plt.savefig("graph.png")
     # plt.show()
+    db = add_atc_db_eng(model, atc, db)
+    db = add_family_db_eng(model, family, db)
     db_save(db)
+    
+
 
 if __name__ == "__main__":
     main()
--- DB/db_infer.py
+++ DB/db_infer.py
@@ -3,16 +3,34 @@ import pickle
 import networkx as nx
 import pandas as pd
 from openpyxl import Workbook
+import re
+from deep_translator import MyMemoryTranslator
+import difflib
 
 def loadDB():
-    return pickle.load(open('drug_db_add_api_1117.pickle', 'rb'))
+    return pickle.load(open('drug_db_eng_1228.pickle', 'rb'))
 
+def deal_name(name):
+    '''
+    a=a盐=×盐a = a×盐= a盐1盐2=盐1盐2a= a×盐1盐2=×盐1盐2a；盐可换成酸、金属
+    金属：钠、钙、镁、钾、铵、铁、银、铋
+    '''
+    if '酸' in name:
+        name_dealed = re.sub(r"^.*酸", "", name)
+    
+'''
+def judgeByRegulaity(drug_name, node):
+    if '酸' in drug_name:
+        drug_name_dealed = re.sub(r"^.*酸", "", drug_name)
+    if 
+'''
 def getSimilarNode(drug_name, model):
     drug_vector = model.encode(drug_name)
     db = loadDB()
     maxSimilar = 0.0
     similarNode = db
     for node in db.nodes():
+        '''
         if node == drug_name:
             return node, '规则'
         elif node in drug_name:
@@ -28,12 +46,13 @@ def getSimilarNode(drug_name, model):
                 '的浸膏' in name or '的流浸膏' in name or '的提取物' in name or '艾司' in name or\
                 name.encode('utf-8').isalpha() or '维生素' not in node and name.isdigit():
                     return node, '规则'
-    
-        sim = util.cos_sim(db.nodes[node]['vector'], drug_vector)
+        '''
+        #sim = util.cos_sim(db.nodes[node]['vector'], drug_vector)
+        sim = difflib.SequenceMatcher(None, drug_name, node).quick_ratio()
         if sim > maxSimilar:
             maxSimilar = sim
             similarNode = node
-    return similarNode, '相似度'    
+    return similarNode   
 
 def getAllChild(db, node):
     if not node:
@@ -42,7 +61,7 @@ def getAllChild(db, node):
     children_list = [node]
     for i in range(len(children_list)):
         neighbor = list(db.neighbors(children_list[i]))
-        for nei in neighbor:
+        for nei in neighbor: 
             if nei not in children_set:
                 children_set.add(nei)
                 children_list.append(nei)
@@ -56,9 +75,9 @@ if __name__ == '__main__':
     confict = read_xlsx_confict('../chatgpt/冲突测试.xlsx')
     db = loadDB()
     model = SentenceTransformer('../../../model/biobert-v1.1')
-    
     workbook = Workbook()
-    save_file = "异名表1212_规则_冲突对齐.xlsx"
+    save_file = "无异名表0103_英文_冲突对齐.xlsx"
+    translator = MyMemoryTranslator(source='zh-CN', target='en-GB')
     worksheet = workbook.active
         #每个workbook创建后，默认会存在一个worksheet，对默认的worksheet进行重命名
     worksheet.title = "Sheet1"
@@ -66,12 +85,14 @@ if __name__ == '__main__':
         res = []
         if not pd.isnull(data[0]):
             res = res + data[0].split('；')
+        
         if not pd.isnull(data[1]):
             res = res  + data[1].split('；')
+        
         for r in res:
-            simiNode, regular = getSimilarNode(r, model)
+            simiNode = getSimilarNode(translator.translate(r), model)
             childNode = getAllChild(db, simiNode)
-            worksheet.append([r, simiNode, '；'.join(childNode), regular])
+            worksheet.append([r, simiNode, '；'.join(childNode)])
     workbook.save(filename=save_file)
     
 
